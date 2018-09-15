026764dcad519f9206a9d1d38618000ecb00978b
TheChainCollective <42595698+TheChainCollective@users.noreply.github.com>
2018-09-15 14:32:31 -0400

    Update and rename config.sample.js to config.js

---
+++ push-messaging-and-notifications/config.js
@@ -0,0 +1,5 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAArVeZDZA:APA91bGUZEPcfCsECytJzyatpIiqG55qc9lodwY75c-H_HsQH7USCRPxN5O32KSfVmUb_cD3WJPcWWGBcfjhphIdkZ1J7SP8jdP_IoffhUATko6BljCa2GuI_kN8EBI4_VqfZAPlV4Ei
+'
+};
